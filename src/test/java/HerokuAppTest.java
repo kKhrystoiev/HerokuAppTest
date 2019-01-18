aef4d97773705fd717f947d3a63e7e2ca38b75fb
Kostiantyn Khrystoiev <kostyakhristoyev@gmail.com>
2019-01-18 12:25:41 +0200

    init

---
+++ src/test/java/HerokuAppTest.java
@@ -0,0 +1,40 @@
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class HerokuAppTest {
+    private static WebDriver driver;
+    private static String LINKS_LOCATOR = "//*[@id='content']//a";
+    private static String LINKS_LOCATOR_BY_TEXT = "//*[@id='content']//a[.='$1']";
+
+    @BeforeClass
+    public static void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, SECONDS);
+        driver.get("https://the-internet.herokuapp.com");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test_herokuApp(String arg) {
+//        final String arg = "Broken Images";
+        List<WebElement> listOfElements = driver.findElements(By.xpath(LINKS_LOCATOR));
+        Assert.assertTrue("Link's name not found", listOfElements.stream().map(WebElement::getText).anyMatch(e -> e.equals(arg)));
+        driver.findElement(By.xpath(LINKS_LOCATOR_BY_TEXT.replace("$1", arg))).click();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
